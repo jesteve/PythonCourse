0cc1db10d770058da5de4bcd26d4696acadcacf6
Jerome <esteve@lkb.ens.fr>
2015-03-05 20:48:07 +0100

    Decode solution

---
+++ decode.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def decode(trc):
+    """Quick and dirty Lecroy TRC format decoder."""
+    # See Lecroy manual p. 282
+    start = trc.find('WAVEDESC')
+    trc = trc[start:]
+    nb_of_points   = np.fromstring(trc[60 :64 ], dtype=np.uint32)
+    voltage_gain   = np.fromstring(trc[156:160], dtype=np.float32)
+    voltage_offset = np.fromstring(trc[160:164], dtype=np.float32)
+    horiz_interval = np.fromstring(trc[176:180], dtype=np.float32)
+    horiz_offset   = np.fromstring(trc[180:188], dtype=np.float64)
+    voltage = np.fromstring(trc[346:], dtype=np.int8, count=nb_of_points).astype(np.float)
+    voltage *= voltage_gain
+    voltage += voltage_offset
+    time = np.arange(nb_of_points, dtype=np.float)
+    time *= horiz_interval
+    time += horiz_offset
+    return time, voltage
